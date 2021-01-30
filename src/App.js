c0ca112c945de2ad4c37cf7f5fde99d15c44c5ec
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-01-30 20:06:22 +0900

    App 첫페이지 수정
    ant 기본 레이아웃 적용
    hello api 호출 데이타 표시
    message api 호출 데이타 표시
    인풋박스 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {findAll, hello} from "./api/ApiUtil";
+import {Button, Input, Layout } from "antd";
+import { UserOutlined } from '@ant-design/icons';
+const { Header, Footer, Sider, Content } = Layout;
+
 
 function App() {
+
+    const[state, setState] = useState({name:"",hello:''});
+    const[message, setMessage] = useState("");
+    const[messages, setMessages] = useState([]);
+
+    function handleChange(event){
+        console.log(event.target.value);
+        setMessage(event.target.value);
+    }
+
+    function handleMessageButton() {
+        findAll().then(response=>{
+            setMessages(response);
+        })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout className="layout">
+          <Header className="header">Header</Header>
+          <Content className="content">
+              <div>
+                  <p>
+                      <Button onClick={()=>{
+                          hello().then(response=>{
+                              setState(response);
+                          })
+                      }}>Hello Button!</Button>
+                      {state.name} {state.hello} {state.date} {state.time}
+                  </p>
+
+                  <p>
+                      <Button onClick={handleMessageButton}>Find All!</Button>
+                      {messages.map((d)=>{
+                          console.log(d.message)
+                          return <li key={d.id}>{d.id}. {d.message}</li>
+                      })}
+                  </p>
+
+                  <p>
+                      <Input size="large" placeholder="large size" onChange={handleChange} prefix={<UserOutlined />} />
+                  </p>
+          </div></Content>
+          <Footer className="footer">Footer</Footer>
+      </Layout>
+
   );
 }
 
