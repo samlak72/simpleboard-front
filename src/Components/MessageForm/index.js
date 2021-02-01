c5cdcaf3c062b21da31d7c68c12c6b387db675fb
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-01 23:44:18 +0900

    메세지 입력폼 validation 추가
    Ant Design 라이브러리의 validation기능 추가
    입력값을 가져올때 event에서 가져오는데 ant 라이브리에서 제공하는 기능을 이용하면 validation한 폼값이 리턴값으로 넘어오는 값을 사용하면됨
    입력폼 리셋은 setMessage로는 안되고 ant에서 제공하는 form.resetFields(); 함수를 이용함

--- src/Components/MessageForm/index.js
+++ src/Components/MessageForm/index.js
@@ -1,37 +1,53 @@
-import React, {useState} from 'react';
-import {Input} from "antd";
+import React, {useState,useEffect} from 'react';
+import {Form, Input} from "antd";
 import {UserOutlined} from "@ant-design/icons";
 import {saveMessage} from "../../api/ApiUtil";
 
 function MessageForm(){
 
+    const [form] = Form.useForm();
     const[message, setMessage] = useState("");
 
     function handleChange(event){
         setMessage(event.target.value);
     }
 
-    function handlePressEnter(event){
-        const message = event.target.value;
-        console.log(message);
-        setMessage("");
-        saveMessage(message)
-            .then(result=>{
-                console.log(result);
-                alert(result.message)
-            });
+    async function handlePressEnter(){
+        try{
+            const values = await form.validateFields();
+            setMessage("");
+            saveMessage(values.message)
+                .then(result=>{
+                    alert(result.message)
+                });
+            form.resetFields();
+        }catch(errorInfo){
+            console.log("Faild: " , errorInfo);
+        }
     }
 
     return(
         <p>
-            <Input
-                size="large"
-                placeholder="Input message!!"
-                onChange={handleChange}
-                onPressEnter={handlePressEnter}
-                value={message}
-                prefix={<UserOutlined />}
-            />
+            <Form form={form} name={"messageForm"}>
+                <Form.Item
+                    name={"message"}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input message!",
+                        }
+                    ]}
+                >
+                    <Input
+                        size="large"
+                        placeholder="Input message!!"
+                        onChange={handleChange}
+                        onPressEnter={handlePressEnter}
+                        value={message}
+                        prefix={<UserOutlined />}
+                    />
+                </Form.Item>
+            </Form>
         </p>
     )
 }
