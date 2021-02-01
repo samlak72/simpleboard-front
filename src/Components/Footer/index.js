12a52b4953c6ebc7316f6ef3dc3ae8eb27769c32
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-01 12:12:51 +0900

    Footer 컴포넌트 생성

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+import {Layout} from "antd";
+
+function Footer(){
+
+    return(
+        <Layout.Footer className="footer">
+            Samla Softward
+        </Layout.Footer>
+    )
+}
+
+export default Footer;
