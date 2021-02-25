ee38cdba19bd855b1816b171d0fc7b304bb6cc35
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-25 23:09:46 +0900

    Context Provider 적용
    Hello, Message context provider 적용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from "./Components/Footer";
 import Hello from "./Components/Hello";
 import Message from "./Components/Message";
 import MessageForm from "./Components/MessageForm";
+import {HelloProvider} from "./context/hello-context";
+import {MessageProvider} from "./context/message-context";
 
 const { Content } = Layout;
 
@@ -15,9 +17,14 @@ function App() {
       <Layout className="layout">
           <Header></Header>
           <Content className="content">
-              <Hello></Hello>
-              <MessageForm></MessageForm>
-              <Message></Message>
+              <HelloProvider>
+                  <Hello></Hello>
+              </HelloProvider>
+
+              <MessageProvider>
+                  <MessageForm></MessageForm>
+                  <Message></Message>
+              </MessageProvider>
           </Content>
           <Footer></Footer>
       </Layout>
