d9efcba173db809377d88f4b1a613d74a786fc47
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-28 17:14:41 +0900

    메세지 리스트 수정
    antdesign list 적용
    리스트 클릭시 에디드 모드 변경
    삭제 아이콘추가
    로딩시에 메세지 바로 불러오기

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -1,20 +1,53 @@
-import React, {useState,useContext} from 'react';
-import {Button, Divider, Pagination} from "antd";
-import {findAllSimpleboards} from "../../api/ApiUtil";
+import React, {useEffect, useState} from 'react';
+import {Divider, List, Pagination, Space} from "antd";
+import {deleteSimpleboard, findAllSimpleboards, updateSimpleboard} from "../../api/ApiUtil";
 import {useMessageDispatch, useMessageState} from "../../context/message-context";
+import {CloseCircleTwoTone} from "@ant-design/icons";
 
 function Message(){
 
-    const[messages, setMessages] = useState(
-        {
-            content:[],
-            totalElements:0,
-        });
+    const [page,setPage] = useState(0);
+    const [isEdit, setIsEdit] = useState(false);
 
-    // Message context를 가져온다.
     const messageState = useMessageState();
-    console.log(messageState)
     const messageDispatch = useMessageDispatch();
+
+    useEffect(()=>{
+        const response = findAllSimpleboards(0).then( response =>
+            messageDispatch({type:'messages',payload:response})
+        )
+    },[])
+
+    function handleMouseOver(e){
+        //e.target.innerHtml='x'
+    }
+
+
+    const messageList = <List
+        header={<div><h1>오실록(오늘의 실행 목록)</h1></div>}
+        footer={<div>나는 오늘로 실행한다.</div>}
+        bordered
+        dataSource={messageState.content}
+        renderItem={item=>(
+            <List.Item
+                key={item.id}
+            >
+                <Space size={"small"}>
+                    <CloseCircleTwoTone  data-key={item.id} onClick={handleDeleteMessage} style={{cursor:"pointer"}}/>
+                    <span
+                        data-key={item.id}
+                        contentEditable suppressContentEditableWarning={true}
+                        onMouseOver={handleMouseOver}
+                        onInput={handleMessage}
+                        onBlur={handleMessageInput}
+                        style={{width:'100%'}}
+                    >{item.message}</span>
+                </Space>
+
+            </List.Item>
+        )}
+
+    />
 
     async function handleMessageButton(page) {
         const response = await findAllSimpleboards(page?page:0)
@@ -24,6 +57,7 @@ function Message(){
     async function handlePage(page){
         const response = await findAllSimpleboards(page-1)
         messageDispatch({type:'messages',payload:response})
+        setPage(page);
     }
 
     async function handleMessageContext(page){
@@ -32,22 +66,38 @@ function Message(){
     }
 
     function handleMessage(ele){
-        /*console.log('handle message',ele.target.innerText);
-        const text = '<input value=\''+ele.target.innerText+'\'/>'
-        console.log('handle message',text);
-        ele.target.innerHTML = text
-        ele.target.value=ele.target.innerText*/
+        setIsEdit(true);
     }
+
+    function handleMessageChange(e){
+    }
+
+    async function handleMessageInput(e){
+        console.log('Text inside div', e.currentTarget.textContent,e.currentTarget.dataset.key,isEdit)
+        if(isEdit){
+            await updateSimpleboard({message:e.currentTarget.textContent,id:e.currentTarget.dataset.key});
+            const response = await findAllSimpleboards(page>0?page-1:0)
+            messageDispatch({type:'messages',payload:response})
+            setIsEdit(false);
+        }
+        setIsEdit(false);
+    }
+
+    async function handleDeleteMessage(e){
+        console.log(e.currentTarget.dataset)
+        await deleteSimpleboard({message:e.currentTarget.textContent,id:e.currentTarget.dataset.key})
+        const response = await findAllSimpleboards(page>0?page-1:0)
+        messageDispatch({type:'messages',payload:response})
+    }
+
 
     return (
         <p>
-            <Button onClick={()=>handleMessageButton(0)}>Find All!</Button>
-            <ul onClick={handleMessage}>
-            {messageState.content.map((d)=>{
-                return <li key={d.id} contenteditable="true" onClick={handleMessage}>{d.id}. {d.message}</li>
-            })}
-            </ul>
-            <Button onClick={handleMessageContext}>Fill all context!</Button>
+            {/*<Button onClick={()=>handleMessageButton(0)}>Find All!</Button>*/}
+
+            {messageList}
+
+            {/*<Button onClick={handleMessageContext} >Fill all context!</Button>*/}
             <Divider/>
             <Pagination
                 defaultCurrent={1}
