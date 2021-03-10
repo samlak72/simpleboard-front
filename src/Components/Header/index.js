ace2c852f211daa6d695db8be468adf55f3f1a81
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-03-10 23:44:20 +0900

    헤더 메뉴 추가

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,22 @@
-import {Layout} from "antd";
+import {Layout, Menu} from "antd";
+import {Link} from "react-router-dom";
 
 function Header(){
 
     return(
-        <Layout.Header className="header">Simple Board.</Layout.Header>
+        <>
+            <Layout.Header className="header">
+                <div className={"top-nav g-width"}>
+                    <div className="top-logo" ><h1>Osillok</h1></div>
+                    <Menu className={"top-nav-menu"} theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1"><Link to={"/"}>Home</Link></Menu.Item>
+                        <Menu.Item key="2"><Link to={"/messages"}>Osillok</Link></Menu.Item>
+                        <Menu.Item key="3" style={{"float":"right"}}><Link to={"/signin"}>Sign in</Link></Menu.Item>
+                    </Menu>
+                </div>
+            </Layout.Header>
+        </>
+
     )
 }
 
