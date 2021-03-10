c8fdd35119faed209b4f3701fb0e29ed85a1eef0
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-03-10 23:43:40 +0900

    앱 수정
    라우터 적용
    홈 추가
    로그인 폼 추가

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
 import {Layout} from "antd";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Hello from "./Components/Hello";
 import Message from "./Components/Message";
 import MessageForm from "./Components/MessageForm";
-import {HelloProvider} from "./context/hello-context";
 import {MessageProvider} from "./context/message-context";
+import Signin from "./Components/Signin";
+import Home from "./Components/Home";
 
 const { Content } = Layout;
 
 function App() {
 
   return (
-      <Layout className="layout">
-          <Header></Header>
-          <Content className="content">
-              <HelloProvider>
-                  <Hello></Hello>
-              </HelloProvider>
-
-              <MessageProvider>
-                  <MessageForm></MessageForm>
-                  <Message></Message>
-              </MessageProvider>
-          </Content>
+      <Router>
+          <Layout className="layout">
+              <Header></Header>
+              <Content className="content">
+                  <Switch>
+                      <Route path={"/messages"}>
+                          <MessageProvider>
+                              <MessageForm></MessageForm>
+                              <Message></Message>
+                          </MessageProvider>
+                      </Route>
+                      <Route path={"/signin"}>
+                          <Signin />
+                      </Route>
+                      <Route path={"/"}>
+                          <Home />
+                      </Route>
+                  </Switch>
+                  <div className={"push"}/>
+              </Content>
+          </Layout>
           <Footer></Footer>
-      </Layout>
+      </Router>
+
   );
 }
 
